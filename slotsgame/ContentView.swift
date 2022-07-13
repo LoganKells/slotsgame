e7c046a3344efa1349181a573f033d96a5bb7eea
LoganKells <lkells@outlook.com>
2022-07-13 17:38:50 -0400

    Initial Commit

---
+++ slotsgame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  slotsgame
+//
+//  Created by Logan Kells on 7/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ slotsgame/slotsgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotsgameApp.swift
+//  slotsgame
+//
+//  Created by Logan Kells on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct slotsgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
